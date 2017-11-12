e9efc751a7be1fdc1e25706795816ef1a7a2c069
Yoss <yoseline_96@hotmail.com>
2017-11-12 12:37:34 -0500

    Vinculando archivo css con html

--- js/app.js
+++ js/app.js
@@ -1,34 +1,38 @@
-var animal = document.getElementsByClassName("animal");
+//Declaramos variables
+var image = document.getElementsByClassName("animal");
 var select= document.getElementById("select");
 
-select.onChange = function(){
+// Declaramos la funcion onChange
+function onChange(){
   if(select.value === 'original'){
     for(var i=0; i<image.length; i++){
       image[i].classList.remove('white-black');
       image[i].classList.remove('negative-colors');
       image[i].classList.remove('sepia');
     }
-  }else if (select.value === 'sepia') {
+  }
+  if (select.value === 'sepia') {
     for(var i=0; i<image.length; i++){
       image[i].classList.add('sepia');
       image[i].classList.remove('white-black');
       image[i].classList.remove('negative-colors');
-      image[i].classList.remove('original');
     }
-  }else if (select.value === 'white-black') {
+  }
+  if (select.value === 'white-black') {
     for(var i=0; i<image.length; i++){
       image[i].classList.add('white-black');
       image[i].classList.remove('sepia');
       image[i].classList.remove('negative-colors');
-      image[i].classList.remove('original');
     }
-  }else if (select.value === 'negative-colors') {
+  }
+  if (select.value === 'negative-colors') {
     for(var i=0; i<image.length; i++){
       image[i].classList.add('negative-colors');
       image[i].classList.remove('white-black');
       image[i].classList.remove('sepia');
-      image[i].classList.remove('original');
     }
   }
-
-}
+  // Declaramos las funciones que se emplearan al cargar la página
+window.onload = function(){
+  select.addEventListener('change', onChange);
+};
